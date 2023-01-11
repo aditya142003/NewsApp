7ef8d7a3478651d18b1e392db3bb67ad5bc57900
aditya142003 <adityabhatnagar142003@gmail.com>
2023-01-11 20:30:42 +0530

    Added category to nav bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class NavBar extends Component {
   static propTypes = {};
@@ -8,8 +9,19 @@ export class NavBar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <div className="navbar-brand" href="#">
+            <div className="navbar-brand" to="#">
               News
+            </div>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li><Link className="nav-link active" to="/business">Business</Link></li>
+                <li><Link className="nav-link active" to="/entertainment">Entertainment</Link></li>
+                <li><Link className="nav-link active" to="/">General</Link></li>
+                <li><Link className="nav-link active" to="/health">Health</Link></li>
+                <li><Link className="nav-link active" to="/science">Science</Link></li>
+                <li><Link className="nav-link active" to="/sports">Sports</Link></li>
+                <li><Link className="nav-link active" to="/technology">Technology</Link></li>
+              </ul>
             </div>
           </div>
         </nav>
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,9 +1,21 @@
-import PropTypes from "prop-types";
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Loading from "./Loading";
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+  static defaultProps={
+    country: 'in',
+    category: 'general',
+    pageSize: '6'
+  }
+
+  static propTypes={
+    country: PropTypes.string,
+    category: PropTypes.string,
+    pageSize: PropTypes.number
+  }
+
   constructor() {
     super();
     this.state = {
@@ -16,7 +28,7 @@ export class News extends Component {
 
   async componentDidMount() {
     this.setState({loading: true});
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=b4d047d672bd4402bb75046d558e9373&page=1&pageSize=20&category=general`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b4d047d672bd4402bb75046d558e9373&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
@@ -28,9 +40,9 @@ export class News extends Component {
 
   handlenext = async () => {
     this.setState({loading: true});
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=b4d047d672bd4402bb75046d558e9373&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b4d047d672bd4402bb75046d558e9373&page=${
       this.state.page + 1
-    }&pageSize=20`;
+    }&pageSize=${this.props.pageSize}`;
     if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
     } else {
       let data = await fetch(url);
@@ -44,9 +56,9 @@ export class News extends Component {
   };
   handleprev = async () => {
     this.setState({loading: true});
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=b4d047d672bd4402bb75046d558e9373&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b4d047d672bd4402bb75046d558e9373&page=${
       this.state.page - 1
-    }&pageSize=20`;
+    }&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({
